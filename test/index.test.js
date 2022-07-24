b8fc81ba02739151f32b6c8df6427814d9586fd9
Rodrigo Amaro <rodrigo.amaro.r@gmail.com>
2022-07-24 15:51:20 -0400

    new reporter

--- test/index.test.js
+++ test/index.test.js
@@ -1,27 +1,27 @@
-const request = require('supertest')
+const request = require('supertest');
 const app = require('../app');
 
 describe('API', () => {
   describe('GET /', () => {
     it('should get a json with title Express', (done) => {
-        const res = request(app)
-          .get('/')
-          .expect("Content-Type", /json/)
-          .expect(200)
-          .then(response => {
-            expect(response.body.title).toEqual("Hola a todos!");
-            done();
+      const res = request(app)
+        .get('/')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((response) => {
+          expect(response.body.title).toEqual('Hola a todos!');
+          done();
         })
-        .catch(err => done(err))
+        .catch((err) => done(err));
     });
     it('should post return 201', (done) => {
       const res = request(app)
-          .post('/')
-          .expect(201)
-          .then(response => {
-            done();
+        .post('/')
+        .expect(201)
+        .then((response) => {
+          done();
         })
-        .catch(err => done(err))
+        .catch((err) => done(err));
     });
   });
 });
